f4f614bad4d5b4f5703646e1d80f01d53480ae05
HauntedMilkshake <spaszahov@gmail.com>
2023-05-15 20:52:21 +0300

    made the connection to access, changed some things regarding the template of the project

---
+++ inventory_management/Form1.Designer.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.OleDb;
+using System.Data;
+using System.Configuration;
+namespace inventory_management
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+       
+       
+    }
+}
+
---
+++ inventory_management/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inventory_management
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+        }
+        private static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["MyAccessDB"].ConnectionString;
+        }
+
+        private void AccessDatabaseExample()
+        {
+            string connectionString = GetConnectionString();
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+
+                OleDbCommand command = new OleDbCommand("SELECT * FROM Products", connection);
+
+                OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                // Do something with the data, for example, display it in a DataGridView.
+            }
+
+
+        }
+    }
+}
